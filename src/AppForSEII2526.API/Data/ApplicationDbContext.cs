10c8f9128e9781bc1bd47f6cad1e0d6813174ab8
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-09-27 19:58:11 +0200

    Adding class TypeItem (#14)
    Class correctly implemented

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ using System.Numerics;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<TypeItem> TypeItems { get; set; }
     public DbSet<Class> Classes { get; set; }
     public DbSet<PlanItem> PlanItems { get; set; }
     public DbSet<Plan> Plans { get; set; }
