3cfba7d367faf1129b60078aa3f0ceab6d3f4a1d
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-09-27 19:50:07 +0200

    Adding class Plan (#11)
    Class correctly implemented

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AppForSEII2526.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using AppForSEII2526.API.Models;
+using System.Numerics;
 
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Plan> Plans { get; set; }
+
 }
