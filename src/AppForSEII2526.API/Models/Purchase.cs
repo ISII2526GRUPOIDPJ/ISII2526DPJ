6ee7406cf3c052bbc3d50226d31461d9c875c6ec
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-09-27 20:13:52 +0200

    Class Purchase created

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -4,5 +4,10 @@
     {
         public int Id { get; set; }
         public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Street { get; set; }
+        public decimal Total_price { get; set; }
     }
 }
