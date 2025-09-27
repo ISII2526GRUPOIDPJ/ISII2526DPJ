8c70c921e8d3f131772d3a6fec74e76af127ecf3
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-09-27 19:54:03 +0200

    Adding class PlanItem (#12)
    Class correctly implemented

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,6 +6,6 @@ using System.Numerics;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<PlanItem> PlanItems { get; set; }
     public DbSet<Plan> Plans { get; set; }
-
 }
---
+++ src/AppForSEII2526.API/Models/PlanItem.cs
@@ -0,0 +1,11 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PlanItem
+    {
+        [Key]
+        public int ClassId { get; set; }
+        public string Goal { get; set; }
+        public int PlanId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
