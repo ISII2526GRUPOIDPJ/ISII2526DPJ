8cd58c64963d582bb2a89ff071272eb243b5c461
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-09-28 09:49:18 +0200

    Task1 create class purchase (#18)
    * Class Purchase added
    
    * Class Purchase created
    
    * Migrations deleted
    
    ---------
    
    Co-authored-by: JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,9 +6,11 @@ using System.Numerics;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Purchase> Purchases { get; set; }
     public DbSet<PaymentMethod> PaymentMethods { get; set; }
     public DbSet<TypeItem> TypeItems { get; set; }
     public DbSet<Class> Classes { get; set; }
     public DbSet<PlanItem> PlanItems { get; set; }
     public DbSet<Plan> Plans { get; set; }
 }
+
---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,13 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Street { get; set; }
+        public decimal Total_price { get; set; }
+    }
+}
