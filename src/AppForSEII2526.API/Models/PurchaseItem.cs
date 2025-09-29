3aec69d680586efe6def89fe0ce2036a7b37e8ba
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-09-29 12:24:58 +0200

    Decimals improved

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -5,6 +5,7 @@
         [Key]
         public int ItemId { get; set; }
         public int Amount_bought { get; set; }
+        [Precision(10,2)]
         public decimal Price { get; set; }
         public int PurchaseId { get; set; }
         public Purchase Purchase { get; set; }
