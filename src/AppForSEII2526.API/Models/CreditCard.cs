a904bfd79de64788df0fc29ffee51eb7030fd3e6
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-10-01 18:06:16 +0200

    Classes added as inheritance

---
+++ src/AppForSEII2526.API/Models/CreditCard.cs
@@ -0,0 +1,8 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class CreditCard:PaymentMethod
+    {
+        public int CreditCardNumber { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
