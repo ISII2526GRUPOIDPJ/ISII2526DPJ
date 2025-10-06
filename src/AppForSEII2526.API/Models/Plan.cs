7a841f8775ceca0e25e6f083edc1dab0e18b94e4
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-10-06 13:37:43 +0200

    Plan's attribute Description modified

--- src/AppForSEII2526.API/Models/Plan.cs
+++ src/AppForSEII2526.API/Models/Plan.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public string HealthIssues { get; set; }
         public string Name { get; set; }
 
