66c1714fedc6de8dd424d83145e6ce044deb821e
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-10-15 11:21:07 +0200

    Creating Class's DTO

---
+++ src/AppForSEII2526.API/DTOs/ClassDTOs/ClassForPlanDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace AppForSEII2526.API.DTOs.ClassDTOs
+{
+    public class ClassForPlanDTO
+    {
+        public ClassForPlanDTO(int id, string name, string goal)
+        {
+            Id = id;
+            Name = name;
+            Goal = goal;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Goal { get; set; }
+    }
+}
