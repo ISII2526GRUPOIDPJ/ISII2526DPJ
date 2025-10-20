84ee8cf765bb9628b06e4a7dc3f9e41b7c6955a0
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-10-20 12:28:34 +0200

    Task58 develop GetItemsForPurchase (#61)
    * Controller ItemsController created
    
    * Controller updated and DTO created
    
    * GetItemsForPurchase completed

---
+++ src/AppForSEII2526.API/Controllers/ItemsController.cs
@@ -0,0 +1,50 @@
+﻿using AppForSEII2526.API.DTOs.ItemDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ItemsController> _logger;
+
+        public ItemsController(ApplicationDbContext context, ILogger<ItemsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /*[HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        {
+            if(op2 == 0) {
+                string error = "You can't divide by zero.";
+                _logger.LogError("[" + DateTime.Now + "] Error: " + error);
+                return BadRequest(error);
+            }
+            decimal result = op1 / op2;
+            return Ok(result);
+        }*/
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<ItemForPurchaseDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetItemsForPurchase(string? itemName, string? brandName)
+        {
+            IList<ItemForPurchaseDTO> items = await _context.Items
+                .Include(i => i.Name)
+                .Include(i => i.PurchaseItems)
+                .Where(i => (i.Name.Contains(itemName) || (itemName == null)) && (i.Brand.Name.Equals(brandName) || (brandName == null)))
+                .OrderBy(i => i.Name)
+                .Select(i => new ItemForPurchaseDTO(i.Name, i.Brand.Name, i.Description, i.PurchaseItems.Select(pi => pi.Price).ToList(), i.QuantityAvailableForPurchase))
+                .ToListAsync();
+            return Ok(items);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ItemDTOs/ItemForPurchaseDTO.cs
@@ -0,0 +1,24 @@
+﻿
+using SQLitePCL;
+
+namespace AppForSEII2526.API.DTOs.ItemDTOs
+{
+    public class ItemForPurchaseDTO
+    {
+        public ItemForPurchaseDTO(string name, string brand, string description, IList<decimal> purchaseItems, int quantity)
+        {
+            Name = name;
+            Brand = brand;
+            Description = description;
+            PurchaseItems = purchaseItems;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public IList<decimal> PurchaseItems { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
