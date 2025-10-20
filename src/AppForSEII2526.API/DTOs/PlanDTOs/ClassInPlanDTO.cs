2dbf25da607c7d9c63b9984528513ac7d50020a5
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-10-20 18:57:55 +0200

    Task59 Develop GetPlan (#65)
    * Adding PlanController with GetPlan method
    
    * Adding ClassInPlanDTO for plan classes display
    
    * Adding GetPlanDTO for plan details display

---
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/ClassInPlanDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class ClassInPlanDTO
+    {
+        public ClassInPlanDTO(string name, IList<string> typeItemNames, decimal price, DateTime date, string goal)
+        {
+            Name = name;
+            TypeItemNames = typeItemNames;
+            Price = price;
+            Date = date;
+            Goal = goal;
+        }
+
+        public string Name { get; set; }
+        public IList<string> TypeItemNames { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public string Goal { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/GetPlanDTO.cs
@@ -0,0 +1,32 @@
+﻿using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class GetPlanDTO
+    {
+        public GetPlanDTO(string userName, string userSurname, DateTime createdDate, decimal totalPrice,
+            string name, string description, int weeks, string healthIssues, IList<ClassInPlanDTO> classes)
+        {
+            UserName = userName;
+            UserSurname = userSurname;
+            CreatedDate = createdDate;
+            TotalPrice = totalPrice;
+            Name = name;
+            Description = description;
+            Weeks = weeks;
+            HealthIssues = healthIssues;
+            Classes = classes;
+        }
+
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Weeks { get; set; }
+        public string HealthIssues { get; set; }
+        public IList<ClassInPlanDTO> Classes { get; set; }
+
+    }
+}
