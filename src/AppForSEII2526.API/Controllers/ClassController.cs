70e4b7f4e912a850c57240c2242d05e9e84f13d6
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-10-20 12:22:09 +0200

    Task57 develop get method selectclassesforplan (#60)
    * Creating controller for Class
    
    * Adding Classes Script
    
    * Creating Class's DTO
    
    * Added Include to join Class with TypeItem
    
    * Added Include to join Class with PlanItem
    
    * Updated Class Controller and DTO

---
+++ src/AppForSEII2526.API/Controllers/ClassController.cs
@@ -0,0 +1,56 @@
+﻿using AppForSEII2526.API.DTOs.ClassDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ClassController> _logger;
+
+        public ClassController(ApplicationDbContext context, ILogger<ClassController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<ClassForPlanDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetClassesForPlanning(DateTime? date)
+        {
+            IList<ClassForPlanDTO> classesDTOS = await _context.Classes
+                .Include(c => c.TypeItems)
+                .Where(c => !date.HasValue || c.Date.Date == date.Value.Date)
+                .OrderBy(c => c.Name)
+                .Select(c=>new ClassForPlanDTO(
+                    c.Id,
+                    c.Name,
+                    c.TypeItems.Select(ti => ti.Name).ToList(),
+                    c.Date
+                ))
+                .ToListAsync();
+            return Ok(classesDTOS);
+        }
+
+        //[HttpGet]
+        //[Route("[action]")]
+        //[ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        //[ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        //public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        //{
+            //if (op2 == 0)
+            //{
+            //    string error = "Op2 cannot be 0 to compute a division";
+            //    _logger.LogError(DateTime.Now + " Error: "+ error);
+            //    return BadRequest(error);
+            //}
+
+            //decimal result = op1 / op2;
+            //return Ok(result);
+        //}
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ClassDTOs/ClassForPlanDTO.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace AppForSEII2526.API.DTOs.ClassDTOs
+{
+    public class ClassForPlanDTO
+    {
+        public ClassForPlanDTO(int id, string name, IList<string> typeItemNames, DateTime dateTime)
+        {
+            Id = id;
+            Name = name;
+            TypeItemNames = typeItemNames;
+            DateTime = dateTime;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<string> TypeItemNames { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
