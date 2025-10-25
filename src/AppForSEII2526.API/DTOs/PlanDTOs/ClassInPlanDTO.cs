559f9b44aaf635a0f3c8785e90fcd5c7f17fc643
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-10-25 18:02:14 +0200

    Adding validation attributes to Plan DTOs

--- src/AppForSEII2526.API/DTOs/PlanDTOs/ClassInPlanDTO.cs
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/ClassInPlanDTO.cs
@@ -11,10 +11,16 @@
             Goal = goal;
         }
 
+        [StringLength(50, ErrorMessage = "Class name cannot be longer than 50 characters.")]
+        [MinLength(3, ErrorMessage = "Class name must be at least 3 characters.")]
         public string Name { get; set; }
         public IList<string> TypeItemNames { get; set; }
+
+        [Precision(10, 2)]
         public decimal Price { get; set; }
         public DateTime Date { get; set; }
+
+        [StringLength(200, ErrorMessage = "Goal cannot be longer than 200 characters.")]
         public string Goal { get; set; }
     }
 }
