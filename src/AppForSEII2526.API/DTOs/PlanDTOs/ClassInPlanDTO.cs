92d6aef0693eaf3f2fc3866ecebc53a8f726d822
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-10-27 18:48:17 +0100

    Including Id attribute to ClassInPlanDTO

--- src/AppForSEII2526.API/DTOs/PlanDTOs/ClassInPlanDTO.cs
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/ClassInPlanDTO.cs
@@ -2,14 +2,17 @@
 {
     public class ClassInPlanDTO
     {
-        public ClassInPlanDTO(string name, IList<string> typeItemNames, decimal price, DateTime date, string goal)
+        public ClassInPlanDTO(int id, string name, IList<string> typeItemNames, decimal price, DateTime date, string goal)
         {
+            Id = id;
             Name = name;
             TypeItemNames = typeItemNames;
             Price = price;
             Date = date;
             Goal = goal;
         }
+
+        public int Id { get; set; }
 
         [StringLength(50, ErrorMessage = "Class name cannot be longer than 50 characters.")]
         [MinLength(3, ErrorMessage = "Class name must be at least 3 characters.")]
