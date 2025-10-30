24732f9d967fa5a1a306750ea190706a8ec77c0e
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-10-30 16:24:30 +0100

    Creating a Constructor to Class class

--- src/AppForSEII2526.API/Models/Class.cs
+++ src/AppForSEII2526.API/Models/Class.cs
@@ -3,6 +3,23 @@
     [Index(nameof(Name), nameof(Date), IsUnique = true)]
     public class Class
     {
+        public Class()
+        {
+            TypeItems = new List<TypeItem>();
+            PlanItems = new List<PlanItem>();
+        }
+
+
+        public Class(string name, IList<TypeItem> typeItems, DateTime date, decimal price, int capacity)
+        {
+            Name = name;
+            TypeItems = typeItems;
+            Date = date;
+            Price = price;
+            Capacity = capacity; 
+            PlanItems = new List<PlanItem>();
+        }
+
         public int Id { get; set; }        
         public string Name { get; set; }
         public int Capacity { get; set; }
