33ed65c50864bdc4a42908d1c58ffa0e36581d3b
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-10-31 07:06:03 +0100

    Nearly Implemented

--- src/AppForSEII2526.Maui/MauiProgram.cs
+++ src/AppForSEII2526.Maui/MauiProgram.cs
@@ -1,23 +1,43 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Localization;
+using System.Globalization;
+using Microsoft.Extensions.Logging;
 
-namespace AppForSEII2526.Maui {
-    public static class MauiProgram {
-        public static MauiApp CreateMauiApp() {
+namespace AppForSEII2526.Maui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
             var builder = MauiApp.CreateBuilder();
+
+            // Recupera el idioma guardado o usa inglés por defecto
+            var savedCulture = SecureStorage.Default.GetAsync("AppCulture").Result ?? "en";
+            var culture = new CultureInfo(savedCulture);
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+
             builder
                 .UseMauiApp<App>()
-                .ConfigureFonts(fonts => {
+                .ConfigureFonts(fonts =>
+                {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
             builder.Services.AddMauiBlazorWebView();
 
+            // Habilitar localización
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources/Languages");
+
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
+            var app = builder.Build();
+
+            return app;
         }
     }
 }
