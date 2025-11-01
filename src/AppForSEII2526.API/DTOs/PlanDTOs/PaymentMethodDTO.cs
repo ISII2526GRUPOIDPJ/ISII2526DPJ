3633e4fb5185c9797e84351d0905f6b28586985d
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-11-01 15:15:06 +0100

    Task 79 Develop CreatePlan (#87)
    * Making Goal attribute in ClassInPlanDTO optional
    
    * Including Id attribute to ClassInPlanDTO
    
    * Adding PaymentMethodDTO for payment method handling
    
    * Adding CreatePlanDTO for plan creation requests
    
    * Implementing CreatePlan
    
    * Fix '500' error in CreatePlan by including Include/ThenInclude

---
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/PaymentMethodDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace AppForSEII2526.API.DTOs.PlanDTOs
+{
+    public class PaymentMethodDTO
+    {
+        public PaymentMethodDTO() { }
+
+        public PaymentMethodDTO(int id, string type, string displayInfo)
+        {
+            Id = id;
+            Type = type;
+            DisplayInfo = displayInfo;
+        }
+
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string DisplayInfo { get; set; }
+    }
+}
