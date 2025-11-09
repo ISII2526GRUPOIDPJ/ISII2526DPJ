3e3cfc118cbcba77b95bd1400586b99f9eeccf2d
DANIEL FÉLIX MOYA <Daniel.Felix@alu.uclm.es>
2025-11-09 21:35:24 +0100

    - Some changes done (PaymentMethod and PurchaseItems to be fixed)

--- test/AppForSEII2526.UT/PurchaseController_test/GetPurchase_test.cs
+++ test/AppForSEII2526.UT/PurchaseController_test/GetPurchase_test.cs
@@ -14,6 +14,23 @@ namespace AppForSEII2526.UT.PurchaseController_test
     public class GetPurchase_test : AppForSEII25264SqliteUT
     {
         public GetPurchase_test() {
+            var brands = new List<Brand>() {
+                    new Brand("Nike"),
+                    new Brand("Adidas")
+            };
+
+            var types = new List<TypeItem>() {
+                    new TypeItem("Yoga"),
+                    new TypeItem("Cardio"),
+                    new TypeItem("Strenght")
+            };
+
+            var items = new List<Item>() {
+                    new Item("Yoga mat for exercises", "Yoga Mat", 25, 10, 5, 20, types[0], brands[0]),
+                    new Item("Running Shoes", "Running Shoes", 80, 15, 8, 70, types[1], brands[1]),
+                    new Item("Shirt for doing exercises", "Sports Shirt", 100, 0, 6, 85, types[2], brands[0])
+            };
+
             var purchase = new List<Purchase>() {
                 new Purchase("Madrid", "Spain", DateTime.Parse("2024-01-10"), "Gym equipment", "Main Street 123", 150, 1),
                 new Purchase("Barcelona", "Spain", DateTime.Parse("2024-01-12"), "Sports clothing", "Park Avenue 456", 89.99m, 2)
@@ -51,8 +68,8 @@ namespace AppForSEII2526.UT.PurchaseController_test
 
             var controller = new PurchaseController(_context, logger);
 
-            var expectedPurchase1 = new PurchaseDTO("Madrid", "Spain", "Main Street 123", 150, "Gym equipment", 1, 0);
-            var expectedPurchase2 = new PurchaseDTO("Barcelona", "Spain", "Park Avenue 456", 89.99m, "Sports clothing", 2, 0);
+            var expectedPurchase1 = new PurchaseDTO("Madrid", "Spain", "Main Street 123", 150, "Gym equipment", 1, new PurchaseItemsDTO("Yoga Mat", "Nike", 10, 25);
+            var expectedPurchase2 = new PurchaseDTO("Barcelona", "Spain", "Park Avenue 456", 89.99m, "Sports clothing", 2, new PurchaseItemsDTO("Running Shoes", "Adidas", 15, 80);
 
             //Act
             var result = await controller.GetPurchase(0);
