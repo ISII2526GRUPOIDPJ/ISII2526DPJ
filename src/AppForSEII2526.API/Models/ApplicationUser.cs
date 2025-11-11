f62600c83006a1f1c61ba87ee6ac1e23bd74247d
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-11-11 20:03:05 +0100

    Task101 Unit Test for GetPurchase() - Purchase Controller (#123)
    * - Space created
    - Purchase constructor added
    
    * - Class updated
    - DTOs moved
    
    * - Some changes done (PaymentMethod and PurchaseItems to be fixed)
    
    * - Method finished (some Equals methods added)

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -15,7 +15,13 @@ public class ApplicationUser : IdentityUser {
         Name = name;
         Surname = surname;
     }
-
+    public ApplicationUser(int id, string name, string surname)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+    }
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
     public IList<PaymentMethod> PaymentMethods { get; set; }
--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -2,6 +2,17 @@
 {
     public class Purchase
     {
+        public Purchase() { }
+        public Purchase(string city, string country, DateTime date, string description, string street, decimal total_price, PaymentMethod paymentMethod)
+        {
+            City = city;
+            Country = country;
+            Date = date;
+            Description = description;
+            Street = street;
+            Total_price = total_price;
+            PaymentMethod = paymentMethod;
+        }
         public int Id { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
