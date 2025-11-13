4dffbe2485d996e2591177b6017ce7c3e35cfdb2
DANIEL FÉLIX MOYA <Daniel.Felix@alu.uclm.es>
2025-11-13 18:50:27 +0100

    - Class created

---
+++ test/AppForSEII2526.UT/PurchaseController_test/CreatePurchase_test.cs
@@ -0,0 +1,18 @@
+﻿using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.DTOs.PurchaseDTOs;
+using AppForSEII2526.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UT.PurchaseController_test
+{
+    internal class CreatePurchase_test : AppForSEII25264SqliteUT
+    {
+    }
+}
