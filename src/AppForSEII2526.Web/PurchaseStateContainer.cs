369234a2af05ba0520667ef3374046dc8b69f9d7
DANIEL FÉLIX MOYA <Daniel.Felix@alu.uclm.es>
2025-11-23 15:32:13 +0100

    - Methods added (missing specific items)

--- src/AppForSEII2526.Web/PurchaseStateContainer.cs
+++ src/AppForSEII2526.Web/PurchaseStateContainer.cs
@@ -1,6 +1,37 @@
-﻿namespace AppForSEII2526.Web
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
 {
     public class PurchaseStateContainer
     {
+        public CreatePurchaseDTO Purchase {  get; private set; } = new CreatePurchaseDTO() {
+            PurchaseItems = new List<PurchaseItemsDTO>()
+        };
+
+        public event Action? OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddItemToPurchase(PurchaseItemsDTO item)
+        {
+            
+        }
+
+        public void RemoveItemFromPurchase(PurchaseItemsDTO item)
+        {
+            Purchase.PurchaseItems.Remove(item);
+        }
+
+        public void ClearCart()
+        {
+            Purchase.PurchaseItems.Clear();
+        }
+
+        public void PurchaseProcessed()
+        {
+            Purchase = new CreatePurchaseDTO()
+            {
+                PurchaseItems = new List<PurchaseItemsDTO>()
+            };
+        }
     }
 }
