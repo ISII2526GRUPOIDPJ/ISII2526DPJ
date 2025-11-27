9e9315b6d8a2fe116901c72f434b684a611cfc79
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-11-27 19:12:04 +0100

    Scripts for tests related to Filters of ClassesForPlanning

---
+++ src/AppForSEII2526.API/Data/dbo.Classes.ForPlanning_NoCapacity.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Classes] ON
+
+INSERT INTO [dbo].[Classes] ([Id], [Name], [Capacity], [Date], [Price])
+VALUES
+(3, N'Strength Training', 0, DATEADD(DAY, 2, CAST(GETDATE() AS DATETIME)), CAST(20.00 AS Decimal(10,2)));
+
+SET IDENTITY_INSERT [dbo].[Classes] OFF
