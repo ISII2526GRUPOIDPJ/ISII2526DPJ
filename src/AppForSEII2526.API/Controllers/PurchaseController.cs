304a5dff4a6aec4bf5e313a79180dda9c0e0fe2a
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-12-01 12:13:06 +0100

    - PaymentMethodDTO and instances to the payment methods fixed (instances of PaymentMethodDTO need to be fixed)

--- src/AppForSEII2526.API/Controllers/PurchaseController.cs
+++ src/AppForSEII2526.API/Controllers/PurchaseController.cs
@@ -33,7 +33,7 @@ namespace AppForSEII2526.API.Controllers
                     p.Street,
                     p.Total_price,
                     p.Description,
-                    new PaymentMethodDTO(p.PaymentMethod.Id, "Type"),
+                    new PaymentMethodDTO(p.PaymentMethod.Id, p.PaymentMethod.GetType().Name, p.PaymentMethod.Description),
                     p.PurchaseItems.Select(pi => new PurchaseItemsDTO(pi.Item.Name, pi.Item.Brand.Name, pi.Item.QuantityAvailableForPurchase, pi.Item.PurchasePrice)).ToList()
                 ))
                 .FirstOrDefaultAsync();
--- src/AppForSEII2526.API/DTOs/PlanDTOs/PaymentMethodDTO.cs
+++ src/AppForSEII2526.API/DTOs/PlanDTOs/PaymentMethodDTO.cs
@@ -4,20 +4,23 @@
     {
         public PaymentMethodDTO() { }
 
-        public PaymentMethodDTO(int id, string type)
+        public PaymentMethodDTO(int id, string type, string description)
         {
             Id = id;
             Type = type;
+            Description = description;
         }
 
         public int Id { get; set; }
         public string Type { get; set; }
+        public string Description { get; set; } 
 
         public override bool Equals(object? obj)
         {
             return obj is PaymentMethodDTO dTO &&
                    Id == dTO.Id &&
-                   Type == dTO.Type;
+                   Type == dTO.Type &&
+                   Description == dTO.Description;
         }
     }
 }
