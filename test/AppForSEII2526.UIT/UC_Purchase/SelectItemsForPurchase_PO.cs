852035f52990e8f687c02b8bbdc2fb23e151bf99
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-12-03 11:04:10 +0100

    inputBrand added

--- test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_PO.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_PO.cs
@@ -9,13 +9,18 @@ namespace AppForSEII2526.UIT.UC_Purchase
     public class SelectItemsForPurchase_PO : PageObject {
     
         By inputName = By.Id("inputName");
+        By inputBrand = By.Id("inputBrand");
         protected SelectItemsForPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) {
         }
 
-        public void SearchItems(string name) {
+        public void SearchItems(string name, string brand) {
             WaitForBeingClickable(inputName);
             _driver.FindElement(inputName).SendKeys(name);
             //_driver.FindElement().Click();
+
+            if (brand == "") brand = "All";
+            SelectElement selectElement = new SelectElement(_driver.FindElement(inputBrand));
+            selectElement.SelectByText(brand);
         }
     }
 }
