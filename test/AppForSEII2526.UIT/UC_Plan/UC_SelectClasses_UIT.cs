7ffe6dbeacc7e81b086d302c361a052174104385
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-12-03 10:36:01 +0100

    Create class for implementing the UIT test cases

---
+++ test/AppForSEII2526.UIT/UC_Plan/UC_SelectClasses_UIT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Plan
+{
+    public class UC_SelectClasses_UIT : UC_UIT
+    {
+        public UC_SelectClasses_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
