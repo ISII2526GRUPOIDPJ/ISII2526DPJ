ec099f6f0ca6ba75df6f738c91158ec0ed4c86ff
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-12-03 10:58:05 +0100

    Implementing ids for the Page Object and creating SearchPlan method

--- test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_PO.cs
+++ test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_PO.cs
@@ -8,8 +8,21 @@ namespace AppForSEII2526.UIT.UC_Plan
 {
     public class SelectClassesForPlan_PO : PageObject
     {
+        By inputType = By.Id("typeSelect");
+        By inputDate = By.Id("selectedDate");
+
+        By buttonSearchPlan = By.Id("searchPlan");
+
         public SelectClassesForPlan_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
+        }
+
+        public void SearchPlan(String type) 
+        {
+            // Wait for the web element to be clickable
+            WaitForBeingClickable(inputType);
+            _driver.FindElement(inputType).SendKeys(type);
+            _driver.FindElement(buttonSearchPlan).Click();
         }
     }
 }
