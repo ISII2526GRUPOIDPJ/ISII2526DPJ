53b3e51056afc52d50d3bfbb0907041a2f8bcb03
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-12-03 10:37:04 +0100

    Folder and class created

---
+++ test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseItems_UIT.cs
@@ -0,0 +1,16 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Purchase
+{
+    public class UC_PurchaseItems_UIT : UC_UIT
+    {
+        public UC_PurchaseItems_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
