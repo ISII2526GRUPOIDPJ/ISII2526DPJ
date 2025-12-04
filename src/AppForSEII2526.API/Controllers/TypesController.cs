77a569b1668b44f68e8b6871fb2960331e6ff40e
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-12-04 11:47:18 +0100

    Task198 implement get for getting types of classes (#199)
    * Implementing a GET for getting the types of classes
    
    * Fix problem
    
    * removing logger message

---
+++ src/AppForSEII2526.API/Controllers/TypesController.cs
@@ -0,0 +1,42 @@
+﻿namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ClassController> _logger;
+
+        public TypesController(ApplicationDbContext context, ILogger<ClassController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetAllClassTypes()
+        {
+            try
+            {
+                IList<string> types = await _context.TypeItems
+                    .OrderBy(t => t.Name)
+                    .Select(t => t.Name)
+                    .Distinct()
+                    .ToListAsync();
+
+                if (types == null || types.Count == 0)
+                {
+                    return Ok(new List<string>());
+                }
+
+                return Ok(types);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
