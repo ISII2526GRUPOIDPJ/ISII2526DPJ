bd1c3cca57ee19c476634b44d9bd5996dced564d
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-12-04 16:35:11 +0100

    Implementing date filter

--- test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_PO.cs
+++ test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_PO.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenQA.Selenium.Support.UI;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AppForSEII2526.UIT.UC_Plan
 {
@@ -19,17 +20,26 @@ namespace AppForSEII2526.UIT.UC_Plan
         {
         }
 
-        public void SearchPlan(string type) 
+        public void SearchPlan(string type, DateTime? date = null) 
         {
             // Wait for the web element to be clickable
             WaitForBeingClickable(inputType);
             //_driver.FindElement(inputType).SendKeys(type);
 
             // Select the type from the dropdown
-            if (type == "") type = "All";
+            if (type == "") type = "";
             SelectElement selectType = new SelectElement(_driver.FindElement(inputType));
             selectType.SelectByValue(type);
-            
+
+
+            //Date
+            if (date.HasValue)
+            {
+                var dateString = date.Value.ToString("dd/MM/yyyy");
+                var dateInput = _driver.FindElement(inputDate);
+                dateInput.Clear();
+                dateInput.SendKeys(dateString);
+            }
 
             _driver.FindElement(buttonSearchPlan).Click();
         }
