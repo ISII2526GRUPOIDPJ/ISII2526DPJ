c787e9fe0d8b43161dce461b47735f4992fc77b5
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-12-04 11:33:48 +0100

    Implementing a GET for getting the types of classes

---
+++ src/AppForSEII2526.API/Controllers/TypesController.cs
@@ -0,0 +1,38 @@
+﻿namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ClassController> _logger;
+
+        public TypesController(ApplicationDbContext context, ILogger<ClassController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetAllClassTypes()
+        {
+            try
+            {
+                IList<string> types = await _context.TypeItems
+                    .OrderBy(t => t.Name)
+                    .Select(t => t.Name)
+                    .Distinct()
+                    .ToListAsync();
+
+                return Ok(types);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting class types");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
