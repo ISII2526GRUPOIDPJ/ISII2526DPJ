154001c7db5925a6174d1d6e0835dd15257b625b
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-12-07 18:36:47 +0100

    Task194 User interface testing for selectclassesforplan (#200)
    * Updating the url for running my web app and updating the components for testing
    
    * Create class for implementing the UIT test cases
    
    * Creating the PageObject to interact with SelectClassesForPlan
    
    * Including an ID to the type
    
    * Implementing ids for the Page Object and creating SearchPlan method
    
    * Implementing Selecting by Type
    
    * Changing namespace
    
    * Implementing some ids and checking of the list of classes
    
    * Prov
    
    * Fixing types scroller
    
    * Implementing date filter
    
    * Working test Filter
    
    * Simplifying Filter test
    
    * Setting fixed hours for classes' scripts
    
    * Implementing an id for the errors in SelectClassesForPlan
    
    * Implementing function to handle the errors
    
    * Implementing AF1_UC31_3-4 and AF2_UC31_5
    
    * Adding comments and fixing script

---
+++ test/AppForSEII2526.UIT/UC_Plan/SelectClassesForPlan_PO.cs
@@ -0,0 +1,83 @@
+﻿using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AppForSEII2526.UIT.UC_Plan
+{
+    public class SelectClassesForPlan_PO : PageObject
+    {
+        By inputType = By.Id("typeSelect");
+        By inputDate = By.Id("selectedDate");
+
+        By buttonSearchPlan = By.Id("searchPlan");
+        By tableClasses = By.Id("tableClassesForPlan");
+
+        By errorShownBy = By.Id("ErrorsShown");
+
+        public SelectClassesForPlan_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public void SearchPlan(string type, string date = "")
+        {
+            try
+            {
+                WaitForBeingClickable(inputType);
+
+                // Needed for the dropdown to load its options
+                var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+                wait.Until(driver =>
+                {
+                    var select = new SelectElement(driver.FindElement(inputType));
+                    return select.Options.Count > 0;
+                });
+
+                if (string.IsNullOrEmpty(type))
+                    type = "All";
+
+                var selectElement = new SelectElement(_driver.FindElement(inputType));
+                selectElement.SelectByText(type);
+
+                if (date != "")
+                {
+                    var dateInput = _driver.FindElement(inputDate);
+                    dateInput.Clear();
+
+                    // To transform it into DD/MM/YYYY
+                    if (DateTime.TryParse(date, out DateTime dateValue))
+                    {
+                        dateInput.SendKeys(dateValue.ToString("dd/MM/yyyy"));
+                    }
+                    else
+                    {
+                        dateInput.SendKeys(date);
+                    }
+                }
+
+
+                _driver.FindElement(buttonSearchPlan).Click();
+            }
+            catch (Exception ex)
+            {
+                _output.WriteLine($"Error en SearchPlan: {ex.Message}");
+                throw;
+            }
+        }
+
+        public bool CheckListOfClasses(List<string[]> expectedClasses)
+        {
+            return CheckBodyTable(expectedClasses, tableClasses);
+        }
+
+        public bool CheckMessageError(string errorMessage)
+        {
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"actual Message shown:{actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/UC_Plan/UC_CreatePlan_UIT.cs
@@ -0,0 +1,154 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Plan
+{
+    public class UC_CreatePlan_UIT : UC_UIT
+    {
+        private SelectClassesForPlan_PO selectClassesForPlan_PO;
+        private const string className1 = "Cardio Blast";
+        private const string classType1 = "Cardio";
+        private readonly DateTime classDate1 = DateTime.Today.AddDays(1).AddHours(7); // Today+1, 7:00
+        private const string price1 = "10";
+
+        private const string className2 = "Strength Training";
+        private const string classType2 = "Strength";
+        private readonly DateTime classDate2 = DateTime.Today.AddDays(2).AddHours(8); // Today+2, 8:00
+        private const string price2 = "20";
+
+        private const string className3 = "Yoga";
+        private const string classType3 = "Yoga";
+        private readonly DateTime classDate3 = DateTime.Today.AddDays(2).AddHours(9); // Today+2, 9:00
+        private const string price3 = "12";
+
+        /*
+        private const string className2 = "Strength Training";
+        private const string classType2 = "Strength";
+        private readonly DateTime classDate2 = new DateTime(2025, 12, 06, 08, 00, 00);
+        private const string price2 = "20.00";
+        */
+
+        public UC_CreatePlan_UIT(ITestOutputHelper output) : base(output)
+        {
+            selectClassesForPlan_PO = new SelectClassesForPlan_PO(_driver, _output);
+        }
+
+        /*
+        private void Precondition_perform_login() {
+            Perform_login("email","passwrod");
+        }
+        */
+
+        private void InitialStepsForCreatingPlan()
+        {
+            //Precondition_perform_login();
+            Initial_step_opening_the_web_page();
+            selectClassesForPlan_PO.WaitForBeingVisible(By.Id("CreatePlan"));
+            _driver.FindElement(By.Id("CreatePlan")).Click();
+        }
+
+        // Use dbo.Classes.ForPlanning_AllAvailable.sql & dbo.TypeItems.ForPlanning_AllAvailable.sql to have the classes in the BD
+        [Fact]
+        [Trait("Level Testing", "Functional Testing")]
+        public void UC31_3_AF1_FilterByType()
+        {
+            // Arrange
+            InitialStepsForCreatingPlan();
+
+            var expectedClasses = new List<string[]>
+            {
+                new string[] { className1, classType1, classDate1.ToString("dd/MM/yyyy HH:mm"), price1 }
+            };
+
+            // Act
+            selectClassesForPlan_PO.SearchPlan(classType1);
+
+            // Assert
+            Assert.True(selectClassesForPlan_PO.CheckListOfClasses(expectedClasses));
+        }
+
+        // Use dbo.Classes.ForPlanning_AllAvailable.sql & dbo.TypeItems.ForPlanning_AllAvailable.sql to have the classes in the BD
+        [Fact]
+        [Trait("Level Testing", "Functional Testing")]
+        public void UC31_4_AF1_FilterByDate()
+        {
+            // Arrange
+            InitialStepsForCreatingPlan();
+
+            var expectedClasses = new List<string[]>
+            {
+                new string[] { className1, classType1, classDate1.ToString("dd/MM/yyyy HH:mm"), price1 }
+            };
+
+            // Act
+            string filterDate = DateTime.Today.AddDays(1).ToString("yyyy-MM-dd");
+            selectClassesForPlan_PO.SearchPlan("", filterDate);
+
+            // Assert
+            Assert.True(selectClassesForPlan_PO.CheckListOfClasses(expectedClasses));
+        }
+
+        // Use dbo.Classes.ForPlanning_AllAvailable.sql & dbo.TypeItems.ForPlanning_AllAvailable.sql to have the classes in the BD
+        [Fact]
+        [Trait("Level Testing", "Functional Testing")]
+        public void UC31_5_AF2_DateBeforeToday()
+        {
+            // Arrange
+            InitialStepsForCreatingPlan();
+
+            // Act
+            string pastDate = DateTime.Today.AddDays(-1).ToString("dd/MM/yyyy");
+            selectClassesForPlan_PO.SearchPlan("", pastDate);
+
+
+            // Assert
+            Assert.True(selectClassesForPlan_PO.CheckMessageError("Selected date must be today or later"), $"Error in the message box for test with date: {pastDate}");
+        }
+
+
+    }
+}
+
+
+/*
+ * Hacer script con quantity 0 y meterlo en UC_Plan e indicar el nombre
+[Fact]
+[Trait("LevelTesting", "Functional Testing")]
+public void UC31_noquantity()
+{
+    //Arrange
+    InitialStepsForCreatingPlan();
+    var expectedClasses = new List<string[]>
+    {
+        new string[] { className2, classType2, classDate2.ToString("dd/MM/yyyy HH:mm"), price2 }
+    };
+
+    selectClassesForPlan_PO.SearchPlan("");
+}
+*/
+
+/*
+[Fact]
+[InlineData(className1, classType1, classDate1.ToString("dd/MM/yyyy HH:mm"), price1, "Cardio")]
+[Trait("Level Testing", "Functional Testing")]
+public void UC31_AF1_3_4_5_filter(string className, string classType, string classDate, string classPrice,
+    string filterType)
+{
+    // Arrange
+    InitialStepsForCreatingPlan();
+    var expectedClasses = new List<string[]>
+    {
+        new string[] { className, classType, classDate.ToString("dd/MM/yyyy HH:mm"), price }
+    };
+
+    // Act
+    selectClassesForPlan_PO.SearchPlan(className);
+
+    // Assert
+    Assert.True(selectClassesForPlan_PO.CheckListOfClasses(expectedClasses));
+}
+*/
