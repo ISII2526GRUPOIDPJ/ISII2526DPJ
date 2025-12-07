7cadc25ac723bb08e9790534b490717ad110e143
DANIEL FÉLIX MOYA <Daniel.Felix@alu.uclm.es>
2025-12-07 21:10:07 +0100

    - Async methods for the filters added

---
+++ src/AppForSEII2526.API/Controllers/BrandsController.cs
@@ -0,0 +1,42 @@
+﻿namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ItemsController> _logger;
+
+        public BrandsController(ApplicationDbContext context, ILogger<ItemsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetAllItemBrands()
+        {
+            try
+            {
+                IList<string> brands = await _context.Brands
+                    .OrderBy(b => b.Name)
+                    .Select(b => b.Name)
+                    .Distinct()
+                    .ToListAsync();
+
+                if (brands == null || brands.Count == 0)
+                {
+                    return Ok(new List<string>());
+                }
+
+                return Ok(brands);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
