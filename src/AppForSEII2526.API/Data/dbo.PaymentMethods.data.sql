065bfb9187ab5a59073c1af7ae79b5bb35fe2308
JOSÉ ÁNGEL SOTOCA PULPÓN <JoseAngel.Sotoca@alu.uclm.es>
2025-12-08 09:29:15 +0100

    Updating PaymentMethods script

--- src/AppForSEII2526.API/Data/dbo.PaymentMethods.data.sql
+++ src/AppForSEII2526.API/Data/dbo.PaymentMethods.data.sql
@@ -1,6 +1,8 @@
-﻿SET IDENTITY_INSERT [dbo].[PaymentMethods] ON
+﻿SET IDENTITY_INSERT [dbo].[PaymentMethods] ON;
+
 INSERT INTO [dbo].[PaymentMethods] ([Id], [UserId], [Discriminator], [CreditCardNumber], [ExpirationDate], [TelephoneNumber], [Email]) VALUES 
-(1, N'user1', N'CreditCard', 123456789, '2025-12-31', NULL, NULL),
-(2, N'user2', N'PayPal', NULL, NULL, NULL, N'paypal@email.com'),
-(3, N'user1', N'Bizum', NULL, NULL, 666777888, NULL)
-SET IDENTITY_INSERT [dbo].[PaymentMethods] OFF
+(1, N'1df6eba9-fe88-45e1-9368-5ab1b2f6e905', N'CreditCard', 123456789, '2025-12-31', NULL, NULL),
+(2, N'1df6eba9-fe88-45e1-9368-5ab1b2f6e905', N'PayPal', NULL, NULL, NULL, N'paypal@email.com'),
+(3, N'1df6eba9-fe88-45e1-9368-5ab1b2f6e905', N'Bizum', NULL, NULL, 666777888, NULL);
+
+SET IDENTITY_INSERT [dbo].[PaymentMethods] OFF;
