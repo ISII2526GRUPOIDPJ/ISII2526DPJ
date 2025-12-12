5a0c22541d08efb232569a5d1f0c58537ea93152
DANIEL FÉLIX MOYA <Daniel.Felix@alu.uclm.es>
2025-12-12 19:24:34 +0100

    Quantities fixed

--- src/AppForSEII2526.Web/PurchaseStateContainer.cs
+++ src/AppForSEII2526.Web/PurchaseStateContainer.cs
@@ -17,29 +17,30 @@ namespace AppForSEII2526.Web
         public event Action? OnChange;
         private void NotifyStateChanged() => OnChange?.Invoke();
 
-        public void AddItemToPurchase(PurchaseItemsDTO item)
-        {
-            Purchase.PurchaseItems.Add(item);
+        public void AddItemToPurchase(PurchaseItemsDTO item) {
+            var existing = Purchase.PurchaseItems.FirstOrDefault(i => i.Name == item.Name);
+            if (existing != null) {
+                existing.Quantity += item.Quantity;
+            } else {
+                Purchase.PurchaseItems.Add(item);
+            }
 
             NotifyStateChanged();
         }
 
-        public void RemoveItemFromPurchase(PurchaseItemsDTO item)
-        {
+        public void RemoveItemFromPurchase(PurchaseItemsDTO item) {
             Purchase.PurchaseItems.Remove(item);
 
             NotifyStateChanged();
         }
 
-        public void ClearCart()
-        {
+        public void ClearCart() {
             Purchase.PurchaseItems.Clear();
 
             NotifyStateChanged();
         }
 
-        public void PurchaseProcessed()
-        {
+        public void PurchaseProcessed() {
             Purchase = new CreatePurchaseDTO()
             {
                 PurchaseItems = new List<PurchaseItemsDTO>()
