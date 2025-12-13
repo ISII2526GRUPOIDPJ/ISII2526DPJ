a6ad191ec2453b126caa01d98ae577eeab766fbd
DANIEL FÉLIX MOYA <Daniel.Felix@alu.uclm.es>
2025-12-13 18:51:53 +0100

    - SelectedItemsForPurchase_ PO done - UIT file continued

--- test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_PO.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_PO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +11,56 @@ namespace AppForSEII2526.UIT.UC_Purchase
     
         By inputName = By.Id("inputName");
         By inputBrand = By.Id("inputBrand");
+
+        By buttonSearchItem = By.Id("searchItem");
+        By tableItems = By.Id("tableItemsForPurchase");
+
+        By errorShownBy = By.Id("ErrorsShown");
+
+        By buttonGoToCreatePurchase = By.Id("GoToCreatePurchaseButton");
+
         protected SelectItemsForPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) {
         }
 
         public void SearchItems(string name, string brand) {
             WaitForBeingClickable(inputName);
             _driver.FindElement(inputName).SendKeys(name);
-            //_driver.FindElement().Click();
+
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => {
+                var select = new SelectElement(driver.FindElement(inputBrand));
+                return select.Options.Count > 0;
+            });
 
             if (brand == "") brand = "All";
             SelectElement selectElement = new SelectElement(_driver.FindElement(inputBrand));
             selectElement.SelectByText(brand);
+
+            _driver.FindElement(buttonSearchItem).Click();
+        }
+
+        public bool CheckListOfClasses(List<string[]> expectedItems) {
+            return CheckBodyTable(expectedItems, tableItems);
+        }
+
+        public void ClickGoToCreatePurchase() {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+
+            var btn = wait.Until(
+                ExpectedConditions.ElementToBeClickable(buttonGoToCreatePurchase)
+            );
+
+            btn.Click();
+        }
+
+        public void AddItemToPurchase(string itemName) {
+            WaitForBeingClickable(By.Id("itemToAdd_" + itemName));
+            _driver.FindElement(By.Id("itemToAdd_" + itemName)).Click();
+        }
+
+        public void RemoveItemFromPurchase(string itemName) {
+            WaitForBeingClickable(By.Id("removeItem_" + itemName));
+            _driver.FindElement(By.Id("removeItem_" + itemName)).Click();
         }
     }
 }
--- test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseItems_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseItems_UIT.cs
@@ -7,10 +7,23 @@ using System.Threading.Tasks;
 
 namespace AppForSEII2526.UIT.UC_Purchase
 {
-    public class UC_PurchaseItems_UIT : UC_UIT
-    {
-        public UC_PurchaseItems_UIT(ITestOutputHelper output) : base(output)
-        {
+    public class UC_PurchaseItems_UIT : UC_UIT {
+        private SelectItemsForPurchase_PO SelectItemsForPurchase_PO;
+
+        private const string itemName1 = "Yoga Mat";
+        private const string itemBrand1 = "Nike";
+        private const string itemDescription1 = "Yoga mat for exercises";
+        private const decimal itemPrice1 = 25m;
+        private const int itemQuantity1 = 10;
+
+        private const string itemName2 = "Running Shoes";
+        private const string itemBrand2 = "Adidas";
+        private const string itemDescription2 = "Running shoes";
+        private const decimal itemPrice2 = 80m;
+        private const int itemQuantity2 = 15;
+
+        public UC_PurchaseItems_UIT(ITestOutputHelper output) : base(output) {
+
         }
     }
 }
