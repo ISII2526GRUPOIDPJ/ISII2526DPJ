f6c9dbbadf5384a7b19d1c52d88123ff3dc6a78c
Daniel-Felix-Moya <daniel.felix@alu.uclm.es>
2025-12-13 20:18:11 +0100

    Task196 develop uit for selectitemsforpurchase (#220)
    * - PurchaseStateContainer created and added
    
    * - Methods added (missing specific items)
    
    * - Select created
    - Nevigation updated
    
    * Select more developed
    
    * - DTOs fixed
    - ItemsController fixed
    - Fixing PurchaseController
    
    * - PaymentMethodDTO references fixed
    - Tests need to be fixed
    
    * - DisplayInfo removed from PaymentMethodDTO
    
    * - PaymentMethodDTO and instances to the payment methods fixed (instances of PaymentMethodDTO need to be fixed)
    
    * - PurchaseMethodDTO instances fixed
    
    * - Some more changes
    
    * - GetPurchase_Found arreglado
    
    * - Everything fixed
    
    * - Merged with development to fix conflicts
    
    * Port changed
    
    * Folder and class created
    
    * Creating the PageObject to interact
    
    * Interacting with inputName
    
    * inputBrand added
    
    * - Select Items functioning
    
    * - Repeated items available
    
    * - Async methods for the filters added
    
    * More
    
    * - Commented methods for filters
    
    * - SelectedItemsForPurchase_ PO done
    - UIT file continued
    
    * Changed to public
    
    * - Tests for selecting items done (no items available to be checked)

---
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectItemsForPurchase_PO.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Purchase
+{
+    public class SelectItemsForPurchase_PO : PageObject {
+    
+        By inputName = By.Id("inputName");
+        By itemBrand = By.Id("itemBrand");
+
+        By buttonSearchItem = By.Id("SearchItem");
+        By tableItems = By.Id("tableItemsForPurchase");
+
+        By errorShownBy = By.Id("ErrorsShown");
+
+        By buttonGoToCreatePurchase = By.Id("GoToCreatePurchaseButton");
+
+        public SelectItemsForPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) {
+        }
+
+        public void SearchItems(string name, string brand) {
+            WaitForBeingClickable(inputName);
+            _driver.FindElement(inputName).SendKeys(name);
+
+            if (string.IsNullOrEmpty(brand)) brand = "All";
+
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => new SelectElement(driver.FindElement(itemBrand)).Options.Count > 0);
+
+            SelectElement selectElement = new SelectElement(_driver.FindElement(itemBrand));
+            selectElement.SelectByText(brand);
+
+            _driver.FindElement(buttonSearchItem).Click();
+        }
+
+        public bool CheckListOfItems(List<string[]> expectedItems) {
+            return CheckBodyTable(expectedItems, tableItems);
+        }
+
+        public bool CheckMessageError(string errorMessage) {
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"Actual message shown: {actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+
+        public void ClickGoToCreatePurchase() {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+
+            var btn = wait.Until(
+                ExpectedConditions.ElementToBeClickable(buttonGoToCreatePurchase)
+            );
+
+            btn.Click();
+        }
+
+        public void AddItemToPurchase(string itemName) {
+            WaitForBeingClickable(By.Id("itemToAdd_" + itemName));
+            _driver.FindElement(By.Id("itemToAdd_" + itemName)).Click();
+        }
+
+        public void RemoveItemFromPurchase(string itemName) {
+            WaitForBeingClickable(By.Id("removeItem_" + itemName));
+            _driver.FindElement(By.Id("removeItem_" + itemName)).Click();
+        }
+
+        public bool IsCreatePurchaseButtonAvailable() {
+            try {
+                return _driver.FindElement(buttonGoToCreatePurchase).Displayed;
+            } catch (NoSuchElementException) {
+                return false;
+            }
+        }
+    }
+}
