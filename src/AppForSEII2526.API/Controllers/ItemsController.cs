4582025a9623be77a4d8c85badd08086b63f03a4
Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>
2025-12-17 10:23:55 +0100

    - Change done (Daniel Félix Moya)

--- src/AppForSEII2526.API/Controllers/ItemsController.cs
+++ src/AppForSEII2526.API/Controllers/ItemsController.cs
@@ -21,11 +21,10 @@ namespace AppForSEII2526.API.Controllers
         [HttpGet]
         [Route("[action]")]
         [ProducesResponseType(typeof(IList<ItemForPurchaseDTO>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult> GetItemsForPurchase(string? itemName, string? brandName)
-        {
+        public async Task<ActionResult> GetItemsForPurchase(string? itemName, string? brandName, int minquantity) {
             IList<ItemForPurchaseDTO> items = await _context.Items
                 .Include(i => i.PurchaseItems)
-                .Where(i => (i.Name.Contains(itemName) || (itemName == null)) && (i.Brand.Name.Equals(brandName) || (brandName == null)))
+                .Where(i => (i.Name.Contains(itemName) || (itemName == null)) && (i.Brand.Name.Equals(brandName) || (brandName == null)) && (i.QuantityAvailableForPurchase >= minquantity))
                 .Select(i => new ItemForPurchaseDTO(
                     i.Name,
                     i.Brand.Name,
